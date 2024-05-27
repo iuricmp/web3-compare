1a7becc46820fcfc4a6edd23ec102a3d3396ab48
Iuri Pereira <689440+iuricmp@users.noreply.github.com>
2024-05-27 15:33:03 +0000

    chore: page layout

--- app/balance/page.tsx
+++ app/balance/page.tsx
@@ -3,14 +3,16 @@ export default function Home() {
     <div className="container mx-auto my-8 overflow-x-scroll">
       <div className="mx-auto">
         <h1>getBalance</h1>
-        <table className="table-auto">
+        <table className="table-fixed w-full">
           <thead>
             <tr>
               <th className="px-4 py-2">Library</th>
               <th className="px-4 py-2">Chain</th>
               <th className="px-4 py-2">Method</th>
               <th className="px-4 py-2">Response Type</th>
-              <th className="px-4 py-2">Response Sample</th>
+              <th className="px-4 py-2" style={{ width: 500 }}>
+                Response Sample
+              </th>
               <th className="px-4 py-2">Sample Code</th>
             </tr>
           </thead>
